d0cc348d4c27bf21b0f8d4ac0b06fd854e9de392
Douglas Sousa <douglascamilo@gmail.com>
2019-10-03 00:02:29 -0300

    [Aula 02] - Utilizando features comuns da Expression Language

--- app/src/main/java/br/com/alura/ceep/ui/recyclerview/adapter/ListaNotasAdapter.kt
+++ app/src/main/java/br/com/alura/ceep/ui/recyclerview/adapter/ListaNotasAdapter.kt
@@ -44,12 +44,7 @@ class ListaNotasAdapter(
         ) : RecyclerView.ViewHolder(viewDataBinding.root) {
 
         private lateinit var nota: Nota
-        private val campoDescricao: TextView by lazy {
-            itemView.item_nota_descricao
-        }
-        private val campoFavorita: ImageView by lazy {
-            itemView.item_nota_favorita
-        }
+
         private val campoImagem: ImageView by lazy {
             itemView.item_nota_imagem
         }
@@ -65,23 +60,9 @@ class ListaNotasAdapter(
         fun vincula(nota: Nota) {
             this.nota = nota
             viewDataBinding.setVariable(BR.nota, nota)
-
-            campoDescricao.text = nota.descricao
-            if (this.nota.favorita) {
-                campoFavorita.visibility = VISIBLE
-            } else {
-                campoFavorita.visibility = GONE
-            }
             campoImagem.carregaImagem(nota.imagemUrl)
-            if (nota.imagemUrl.isEmpty()) {
-                campoImagem.visibility = GONE
-            } else {
-                campoImagem.visibility = VISIBLE
-            }
         }
-
     }
-
 }
 
 object DiffCallback : DiffUtil.ItemCallback<Nota>() {
