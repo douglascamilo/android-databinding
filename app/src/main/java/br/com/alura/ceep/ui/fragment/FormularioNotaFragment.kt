cb1d69056d17d3af0cd7dd1ead188956355819d3
Douglas Sousa <douglascamilo@gmail.com>
2019-10-06 23:47:27 -0300

    [Aula 04] - Ajustando demais campos do formulário

--- app/src/main/java/br/com/alura/ceep/ui/fragment/FormularioNotaFragment.kt
+++ app/src/main/java/br/com/alura/ceep/ui/fragment/FormularioNotaFragment.kt
@@ -2,10 +2,6 @@ package br.com.alura.ceep.ui.fragment
 
 import android.os.Bundle
 import android.view.*
-import android.view.View.GONE
-import android.view.View.VISIBLE
-import android.widget.CheckBox
-import android.widget.EditText
 import android.widget.ImageView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
@@ -22,13 +18,11 @@ import br.com.alura.ceep.ui.viewmodel.AppBar
 import br.com.alura.ceep.ui.viewmodel.AppViewModel
 import br.com.alura.ceep.ui.viewmodel.ComponentesVisuais
 import br.com.alura.ceep.ui.viewmodel.FormularioNotaViewModel
-import com.google.android.material.floatingactionbutton.FloatingActionButton
 import kotlinx.android.synthetic.main.formulario_nota.*
 import org.koin.android.viewmodel.ext.android.sharedViewModel
 import org.koin.android.viewmodel.ext.android.viewModel
 
 class FormularioNotaFragment : Fragment() {
-
     private val argumentos by navArgs<FormularioNotaFragmentArgs>()
     private val appViewModel: AppViewModel by sharedViewModel()
     private val notaId by lazy {
@@ -41,15 +35,6 @@ class FormularioNotaFragment : Fragment() {
     private lateinit var notaEncontrada: Nota
     private lateinit var viewDataBinding: FormularioNotaBinding
 
-    private var urlAtual: String = ""
-    private val campoImagem: ImageView by lazy {
-        formulario_nota_imagem
-    }
-
-    private val fabAdicionaImagem: FloatingActionButton by lazy {
-        formulario_nota_fab_adiciona_imagem
-    }
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setHasOptionsMenu(true)
@@ -61,14 +46,14 @@ class FormularioNotaFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         viewDataBinding = FormularioNotaBinding.inflate(inflater, container, false)
+        viewDataBinding.solicitaImagem = View.OnClickListener { solicitaImagem() }
+
         return viewDataBinding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         tentaBuscarNota()
-        configuraFab()
-        configuraCampoImagem()
     }
 
     private fun tentaBuscarNota() {
@@ -89,44 +74,15 @@ class FormularioNotaFragment : Fragment() {
 
     private fun temIdValido() = notaId != 0L
 
-    private fun configuraFab() {
-        fabAdicionaImagem.setOnClickListener {
-            solicitaImagem()
-        }
-    }
-
-    private fun configuraCampoImagem() {
-        configuraVisibilidadeDeComponentes()
-        campoImagem.setOnClickListener {
-            solicitaImagem()
-        }
-    }
-
     private fun solicitaImagem() {
-        CarregaImagemDialog().mostra(requireContext(), urlAtual) { urlNova ->
-            this.urlAtual = urlNova
-            configuraImagem()
-        }
-    }
-
-    private fun configuraVisibilidadeDeComponentes() {
-        if (urlAtual.isEmpty()) {
-            campoImagem.visibility = GONE
-            fabAdicionaImagem.show()
-        } else {
-            campoImagem.visibility = VISIBLE
-            fabAdicionaImagem.hide()
+        CarregaImagemDialog().mostra(requireContext(), notaEncontrada.imagemUrl) { urlNova ->
+            notaEncontrada.imagemUrl = urlNova
+            viewDataBinding.nota = notaEncontrada
         }
     }
 
     private fun inicializaNota(notaEncontrada: Nota) {
         this.notaEncontrada = notaEncontrada
-        urlAtual = this.notaEncontrada.imagemUrl
-    }
-
-    private fun configuraImagem() {
-        configuraVisibilidadeDeComponentes()
-        campoImagem.carregaImagem(urlAtual)
     }
 
     private fun appBarParaEdicao() = ComponentesVisuais(appBar = AppBar(titulo = "Editando nota"))
@@ -141,21 +97,17 @@ class FormularioNotaFragment : Fragment() {
     override fun onOptionsItemSelected(item: MenuItem?): Boolean {
         if (item?.itemId == R.id.formulario_nota_menu_salva) {
             val notaCriada = criaNota()
-            salva(notaCriada)
+
+            notaCriada?.let {
+                salva(notaCriada)
+            }
         }
+
         return super.onOptionsItemSelected(item)
     }
 
-    private fun criaNota(): Nota {
-        val titulo = formulario_nota_titulo.text.toString()
-        val descricao = formulario_nota_descricao.text.toString()
-        val favorita = formulario_nota_favorita.isChecked
-        return notaEncontrada.copy(
-            titulo = titulo,
-            descricao = descricao,
-            favorita = favorita,
-            imagemUrl = urlAtual
-        )
+    private fun criaNota(): Nota? {
+        return viewDataBinding.nota?.copy(id = notaEncontrada.id)
     }
 
     private fun salva(notaNova: Nota) {
